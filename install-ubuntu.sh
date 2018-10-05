7cdb466204179a9d348b169f5f1f87bd40638076
Antti Partanen <aehparta@iki.fi>
2018-10-05 10:45:57 +0300

    almost finished

---
+++ install-ubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be run as root" 
+	exit 1
+fi
+
+cp fwcmd /etc/init.d/fwcmd
+cp fwcmd-rules.conf /etc/fwcmd-rules.conf
+systemctl enable fwcmd
